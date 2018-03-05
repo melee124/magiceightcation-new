074b261ba7e7fb12090be67a1fda101bf65ce93b
Megan Lee <melee124@gmail.com>
2018-03-05 03:39:06 -0800

    Fixed JavaScript & added else if options.

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $("form#travel").submit(function(event) {
+
+  var temperature = parseInt($("input#temperature").val());
+  var distance = parseInt($("input#distance").val());
+  var swim = parseInt($("#swim").val());
+  var location = parseInt($("#location").val());
+  var shoes = parseInt($("#shoes").val());
+  var usernameInput = $("input#username").val();
+
+  $(".username").text(usernameInput);
+
+  if (distance < 100) {
+    $('#staycation').show();
+    $('#cold-weather').hide();
+    $('#swim-yes').hide();
+    $('#jersey-city').hide();
+    $('no-swim').hide();
+  } else if (shoes === 2 && location === 1) {
+    $('#cold-weather').show();
+    $('#staycation').hide();
+    $('#swim-yes').hide();
+    $('#jersey-city').hide();
+    $('no-swim').hide();
+  } else if (swim > 3 && location === 2) {
+    $('#swim-yes').show();
+    $('#staycation').hide();
+    $('#no-swim').hide();
+    $('#jersey-city').hide();
+    $('#cold-weather').hide();
+  } else if (shoes === 1 && location === 3) {
+    $('#jersey-city').show();
+    $('#staycation').hide();
+    $('#no-swim').hide();
+    $('#swim-yes').hide();
+    $('#cold-weather').hide();
+  } else {
+    $('#no-swim').show();
+    $('#staycation').hide();
+    $('#cold-weather').hide();
+    $('#jersey-city').hide();
+    $('swim-yes').hide();
+  }
+    event.preventDefault();
+  });
+});
---
+++ readme.md
@@ -0,0 +1,36 @@
+# _Magiceightcation_
+
+#### _A website which selects travel destinations for users based on their answers to simple questions, February 25, 2018_
+
+#### By _**Megan Lee**_
+
+## Description
+
+_This is a simple psychic website, designed to assist users with difficult decisions in life.  It contains a few forms, and several outcomes based on those forms._
+
+## Setup/Installation Requirements
+
+* _Clone this github file to your desktop using your terminal._
+* _Open using Atom to edit._
+* _Be sure to add, commit, and push any changes you mak.e_
+* _Open in your browser of choice to view the website._
+* _Check your additions using the console feature in the development tools      offered by google._
+
+
+_This code uses HTML, CSS and bootstap, and JavaScript and JQuery._
+
+## Known Bugs
+
+_There is an outcome div for Jersey City, but no actual combination of form answers to get you there.  I was unable to figure out how to create another else if statement to display that option._
+
+## Support and contact details
+
+_If you run into any problems with this code, please contact Megan Lee at melee124@gmail.com_
+
+## Technologies Used
+
+_This project uses HTML, CSS and Bootstrap, and Javascript and JQuery._
+
+### License
+
+Copyright (c) 2018 **_Megan Lee_**
